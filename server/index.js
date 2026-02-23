64e2aa17a698b425af8daf9c40f54e25025c9c92
Mustyyyyyyy <alagbemustapha02@gmail.com>
2026-02-23 09:51:26 +0100

    W

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"], // add your deployed frontend later
+    origin: ["http://localhost:3000", "https://the-penthouse-five.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
